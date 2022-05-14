3b6d80b4d0726c60cc271e662d11b1210da6fb44
TayAntony <tayssaantoniasse123@gmail.com>
2022-05-14 20:26:13 -0300

    ultima aula nlw
    Site finalizado com sucesso, com responsividade e todas as configurações funcionando

--- main.js
+++ main.js
@@ -1,53 +1,85 @@
-window.addEventListener('scroll', onScroll )
+window.addEventListener('scroll', onScroll)
+
 onScroll()
 
-
-
 function onScroll() {
-    showNavOnScroll()
-    showBackToTopOnScroll()
+  showNavOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+
+}
+
+function activateMenuAtCurrentSection(section){
+  // ver se o topo da seção passou da linha
+  const targetLine = scrollY+ innerHeight / 2
+
+  // topo da seção
+  const sectionTop = section.offsetTop
+  // altura da seção
+  const sectionHeight = section.offsetHeight
+  // topo da seção chegou ou passou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // onde a seção termina
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // ver se a base da seção passou da linha
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  // se passou do fim da seção e do topo da proxima
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+
+
+
+  console.log(sectionBoundaries)
 }
 
 function showNavOnScroll() {
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
 }
 
-function showBackToTopOnScroll() {
-    if (scrollY > 1500) {
-        backToTop.classList.add('show')
-    } else {
-        backToTop.classList.remove('show')
-    }
-}
 
 function openMenu() {
-    document.body.classList.add('menu-expanded')
+  document.body.classList.add('menu-expanded')
 }
 
-function closeMenu() {
-    document.body.classList.remove('menu-expanded')
+function closeMenu(){
+  document.body.classList.remove('menu-expanded')
 }
 
-//function ScrollReveal(){
-//   var options = {
-//      reveal: function () {}
-//      }
 
-//      return options }
-//var options = ScrollReveal()
+
 ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-}).reveal(` #home, 
-            #home img, 
-            #home .stats, 
-            #services,
-            #services header,
-            #services .card
-            #about,
-            #about header,
-            #about .content`);
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home,  
+#home img, 
+#home .stats, 
+#services,
+#serices header,
+#services .card,
+#about,
+#about header,
+#about .content
+#contact,
+#contact header,
+#about .content
+`);
